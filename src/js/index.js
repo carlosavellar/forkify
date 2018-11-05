7ab563e2024e097fb8cf98ad0ee0a8c38a9476e0
José Carlos <carlos.avellar@gmail.com>
2018-11-05 11:53:11 -0200

    Module Export Import 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,7 @@
-import { add, multiply, ID, restDivision}  from './views/searchView';
-console.log(`${add(3,5)} --- ${multiply(3,4)} --- ${restDivision(3, 23)}`);
+//Import  1
+// import { add, multiply, ID, restDivision}  from './views/searchView';
+// console.log(`${add(3,5)} --- ${multiply(3,4)} --- ${restDivision(3, 23)}`);
+
+//Import  2
+import * as searchView from './views/searchView';
+console.log(`${searchView.add(3,7)} --- ${searchView.multiply(2,4)} --- ${searchView.restDivision(searchView.ID, 23)}`);
