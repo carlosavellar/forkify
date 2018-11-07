68717c91d8670124c40ecf411614254cdea38dff
José Carlos <carlos.avellar@gmail.com>
2018-11-07 15:33:02 -0200

    Search View: Returning Promise

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -4,12 +4,12 @@ export const getInput = () => elements.searchInput.value;
 const renderRecipe = recipe =>{
     const string = `
           <li>
-              < a class = "results__link results__link--active" href = "${recipe.recipe_id}" >
-              <figure class = "results__fig" >
-              <img src = "${recipe.image_url}" alt = "Test" >
-              </figure> <div class = "results__data" >
-              <h4 class = "results__name" > ${recipe.title} </h4> 
-              <p class = "results__author" > ${recipe.publisher}</p> </div> </a> </li>
+              <a class="results__link results__link--active" href="${recipe.recipe_id}">
+              <figure class="results__fig">
+              <img src="${recipe.image_url}" alt="Test">
+              </figure> <div class="results__data">
+              <h4 class="results__name"> ${recipe.title}</h4> 
+              <p class="results__author"> ${recipe.publisher}</p></div></a></li>
     `;
     elements.searchResults.insertAdjacentHTML("beforeend", string);
 }
