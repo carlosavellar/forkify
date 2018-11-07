8c7344d65233c068a648b665fac2fa20fc88de43
José Carlos <carlos.avellar@gmail.com>
2018-11-07 21:52:43 -0200

    Page Navigation 01

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -31,11 +31,45 @@ const renderRecipe = recipe =>{
     elements.searchResults.insertAdjacentHTML("beforeend", string);
 }
 
+const createButton = (pages, type) => {
+    const button = `
+                <button class="btn-inline results__btn--${type}">
+                    <svg class="search__icon">
+                        <use href = "img/icons.svg#icon-triangle-${type === 'left' ? 'prev' : 'right'}"> </use>
+                    </svg>
+                    <span>Page ${type === 'prev' ? page - 1 : page + 1} </span>
+                </button>
+               <button class="btn-inline results__btn--prev">
+                    <svg class="search__icon">
+                        <use href="img/icons.svg#icon-triangle-left"></use>
+                    </svg>
+                    <span>Page 1</span>
+                </button>
+                <button class="btn-inline results__btn--next">
+                    <span>Page 3</span>
+                    <svg class="search__icon">
+                        <use href="img/icons.svg#icon-triangle-right"></use>
+                    </svg>
+                </button>`;
+}
 
 
+const navPages = (page, totalResult, numPerPage) => {
+    const pages = totalResult / numPerPage;
+    if(page === pages && pages > 1){
+        // Prev button
+    }else if(page === pages){
 
-export const renderResult = recipes =>{
-    recipes.forEach(renderRecipe);
+    }else if(page === pages && pages > 1){
+
+    }
+}
+
+export const renderResult = (recipes, page = 1, resPerPage = 10  )=>{
+    const start = (page - 1) * resPerPage;
+    const end = page * resPerPage;
+     console.log(`${start} end: ${end} `);
+    recipes.slice(start, end).forEach(renderRecipe);
 }
 // export const add = (a, b ) => a + b;
 // export const multiply = (a, b ) => a * b;
